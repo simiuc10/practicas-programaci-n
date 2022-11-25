ccd289edaa795457b9d1ca198193b1e0e2c31d2f
Aarón Ulloa <simiuc00@gmail.com>
2022-11-25 15:10:06 -0600

    condicionales en java

---
+++ Java/condicionales/condicionales/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String escuchar_musica = "Ricardo arjona";
+
+        if (escuchar_musica == "Ricardo arjona"){
+            System.out.println("se ponen las rolas");
+        } else if (escuchar_musica == "alejandro sanz"){
+            System.out.println("ponemos las rolas de alejandro sanz");
+        } else {
+            System.out.println("mientras sea romantica y con melodia las ponemos");
+        }
+
+
+    }
+}
